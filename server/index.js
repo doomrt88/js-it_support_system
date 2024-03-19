f59f4a7c34f9d5a49af6d675735ea5d8c9eabac0
Esperanza Gualdron <espeeranza.g.p@gmail.com>
2024-03-18 21:52:57 -0400

    db setup and connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import controller from "./src/controller/controller.js";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+
+mongoose
+  .connect("mongodb://localhost:28000", {
+    user: "admin",
+    pass: "secureadmin",
+    dbName: "support",
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(cors());
 app.use(express.json());
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,21 +1,70 @@
 import express from "express";
-
+import UserModel from "../models/users.js";
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.send("<strong>USER</strong>");
-});
-
-app.post("/users", (request, response) => {
-  const body = request.body;
-  if (!body) {
-    response
-      .status(400)
-      .json({ error: "bad request", message: "no body provided" });
-    return;
+// Get user by id
+app.get("/users/:id", (request, response) => {
+  const { id } = request.params;
+  if (!id) {
+    // Bad request
+    return response.status(400).json({
+      error: "bad request",
+      message: "No ID provided",
+    });
   }
 
-  response.status(201).json({ user: body });
+  UserModel.findById(id)
+    .then((user) => {
+      return response.status(200).json(user);
+    })
+    .catch((error) => {
+      return response.status(400).json({
+        error: "Error finding user",
+        message: error,
+      });
+    });
+});
+
+// Get all users
+app.get("/users", (_, response) => {
+
+  UserModel.find()
+    .then((user) => {
+      return response.status(200).json(user);
+    })
+    .catch((error) => {
+      return response.status(400).json({
+        error: "Error finding users",
+        message: error,
+      });
+    });
+});
+
+// Create user
+app.post("/users", (request, response) => {
+  const body = request.body;
+
+  if (!body) {
+    // Bad request
+    return response.status(400).json({
+      error: "bad request",
+      message: "No body provided",
+    });
+  }
+
+  UserModel.create(body)
+    .then((user) => {
+      return response.status(201).json({
+        message: "User created",
+        user: user,
+      });
+    })
+    .catch((error) => {
+      return response.status(400).json({
+        error: "Database error",
+        message: error,
+      });
+    });
 });
 
 export default app;
